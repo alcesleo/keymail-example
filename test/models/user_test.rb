5e4f74a5fcc0d08b895001a82425e6e30179e58d
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-08 16:01:22 +0200

    Unique email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,18 @@ require 'test_helper'
 
 describe User do
 
-  it 'must have an email' do
+  it 'has an email' do
     user = Factory.build :user, email: nil
 
     user.must_be :invalid?
     user.errors[:email].must_be :present?
+  end
+
+  it 'has a unique email' do
+    Factory.create :user, email: 'duplicate@example.com'
+    duplicate = Factory.build :user, email: 'duplicate@example.com'
+
+    duplicate.must_be :invalid?
+    duplicate.errors[:email].must_be :present?
   end
 end
