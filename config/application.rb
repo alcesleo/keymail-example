86d95f245827084edd23062e6da4a04112d1158a
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-08 14:20:07 +0200

    Turn off test generators

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,11 @@ module KeymailExample
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # This is NOT a reason not to write tests, I just think the generators are annoying.
+    config.generators do |g|
+      g.test_framework nil
+    end
+
   end
 end
