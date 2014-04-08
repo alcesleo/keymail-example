0f34c254f40ff7cfd86e6f8638b2360dd131029a
Jimmy Börjesson <lagginglion@gmail.com>
2014-04-08 18:28:43 +0200

    Wording

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,15 +2,16 @@ require 'test_helper'
 
 describe User do
 
-  it 'has an email' do
+  it 'must have an email' do
     user = Factory.build :user, email: nil
+
     user.must_have_invalid :email
   end
 
-  it 'has a unique email' do
+  it 'must have a unique email' do
     Factory.create :user, email: 'duplicate@example.com'
-
     duplicate = Factory.build :user, email: 'duplicate@example.com'
+
     duplicate.must_have_invalid :email
   end
 end
