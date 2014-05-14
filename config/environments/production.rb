4e404a584f32c3911334d024ddabc0ff33455cd4
Jimmy Börjesson <lagginglion@gmail.com>
2014-05-14 17:28:46 +0200

    Configuring default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ KeymailExample::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  Rails.application.routes.default_url_options[:host] = 'http://keymail-example.herokuapp.com'
 end
